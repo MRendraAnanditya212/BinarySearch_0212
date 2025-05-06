6c0afe8c6af05637ba46f7e767e24877488bea96
MRendraAnanditya212 <mrendraananditya@gmail.com>
2025-05-06 11:23:18 +0700

    membuat class linked

--- Pertemuan 8/singly.linkedlist.cpp
+++ Pertemuan 8/singly.linkedlist.cpp
@@ -2,18 +2,15 @@
 #include <string.h>
 using namespace std;
 
-
-
-class LinkedList 
+class Node
 {
-    Node *START;
-
 public:
-    LinkedList()
-    {
-        START = NULL;
-    }   
+    int noMhs;
+    Node *next;    
+};
+
     
+
     void addNode()
     {
         int nim;
@@ -34,4 +31,71 @@ public:
             START = nodeBaru;
             return;
         }        
+
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+    
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NUll);
+    }    
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+
+        void traverse()
+        {
+            if (listEmpty())
+            {
+                cout << "\nList Kosong\n";
+            }
+            else 
+            {
+                cout << "\nData didalam list adalah:\n";
+                Node *currentNode = START;
+                while (currentNode != NULL)
+                {
+                    cout << currentNode->noMhs << endl;
+                    currentNode
+                }
 
