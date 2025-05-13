abfba54fb35bf0c3710081dc597876febaeef044
MRendraAnanditya212 <mrendraananditya@gmail.com>
2025-05-13 10:11:19 +0700

    membuat step 1

---
+++ pertemuan 9/doublelinkedlist.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+// membuat struktur untuk object double linkedlist
+struct Node 
+{
+    // deklarasi noMhs dan name untuk menampung data
+    int noMhs;
+    string name;
+    // deklarasi pointer next dan pref untuk penunjukan data sebelum dan sesudah 
+    Node *next;
+    Node *prev;
+};
+
+// deklarasi pointer SMART dan pemberian nilai 
+Node *START = NULL;
+// deklarasi prosedur addNode 
+void addNode()
+{
+
+    
+
+    // insert the new node in the list 
+    // kondisi jika star == null atau noMhs node baru <= noMhs start
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    {
+        // step 2; insert the new node at the beginning
+        // kondisi jika star tidak kosong dan noMhs node baru sama dengan noMhs
+        if (START )
+
+
+    
+
+
+
+
+
+
+
